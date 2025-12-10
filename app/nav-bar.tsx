07cf72531d3761bb4dd79dc16ea7abb4199af289
leoningman <leon.ingman@gmail.com>
2025-12-10 15:25:17 +0100

    refactor: remove redundant code

--- app/nav-bar.tsx
+++
@@ -1,7 +0,0 @@
-export default function NavBar() {
-  return (
-    <header className="rzdv-blue text-white p-4">
-      <h1 className="text-2xl font-bold">My Application</h1>
-    </header>
-  );
-}
